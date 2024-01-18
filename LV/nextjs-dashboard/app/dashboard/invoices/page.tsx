717776712f8b54ea666ccc4adf74e31930917eb5
Jamshed <syed_jamshed_ali@yahoo.com>
2024-01-18 10:36:10 +0500

    remove paging

--- LV/nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ LV/nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -29,7 +29,7 @@ console.log(query);
         <Table query={query} currentPage={currentPage} />
       </Suspense> }
       <div className="mt-5 flex w-full justify-center">
-        { <Pagination totalPages={totalPages} /> }
+        {/*== <Pagination totalPages={totalPages} /> */}
       </div>
     </div>
   );
